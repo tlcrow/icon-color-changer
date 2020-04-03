57182c5a42dd9b98d43fea5ae050db81f2c5402e
TLC <pi_lover852@yahoo.com>
2020-04-03 09:53:14 -0600

    hexToRGB moved to separate file

--- index.js
+++ index.js
@@ -1,131 +1,19 @@
-// class Color extends React.Component {
-// constructor(r, g, b) {
-//     this.state ={
-//         r: r,
-//         g: g,
-//         b: b
-//     }
-//   }
-//   toString() {
-//     return `rgb(${Math.round(this.r)}, ${Math.round(this.g)}, ${Math.round(this.b)})`;
-//   }
-//   set(r, g, b) {
-//       this.setState({r: this.clamp(r), g: this.clamp(g), b: this.clamp(b)});
-//   }
-//   hueRotate(angle = 0) {
-//     angle = angle / 180 * Math.PI;
-//     const sin = Math.sin(angle);
-//     const cos = Math.cos(angle);
-//     this.multiply([
-//       0.213 + cos * 0.787 - sin * 0.213,
-//       0.715 - cos * 0.715 - sin * 0.715,
-//       0.072 - cos * 0.072 + sin * 0.928,
-//       0.213 - cos * 0.213 + sin * 0.143,
-//       0.715 + cos * 0.285 + sin * 0.140,
-//       0.072 - cos * 0.072 - sin * 0.283,
-//       0.213 - cos * 0.213 - sin * 0.787,
-//       0.715 - cos * 0.715 + sin * 0.715,
-//       0.072 + cos * 0.928 + sin * 0.072,
-//     ]);
-//   }
-//   grayscale(value = 1) {
-//     this.multiply([
-//       0.2126 + 0.7874 * (1 - value),
-//       0.7152 - 0.7152 * (1 - value),
-//       0.0722 - 0.0722 * (1 - value),
-//       0.2126 - 0.2126 * (1 - value),
-//       0.7152 + 0.2848 * (1 - value),
-//       0.0722 - 0.0722 * (1 - value),
-//       0.2126 - 0.2126 * (1 - value),
-//       0.7152 - 0.7152 * (1 - value),
-//       0.0722 + 0.9278 * (1 - value),
-//     ]);
-//   }
-//   sepia(value = 1) {
-//     this.multiply([
-//       0.393 + 0.607 * (1 - value),
-//       0.769 - 0.769 * (1 - value),
-//       0.189 - 0.189 * (1 - value),
-//       0.349 - 0.349 * (1 - value),
-//       0.686 + 0.314 * (1 - value),
-//       0.168 - 0.168 * (1 - value),
-//       0.272 - 0.272 * (1 - value),
-//       0.534 - 0.534 * (1 - value),
-//       0.131 + 0.869 * (1 - value),
-//     ]);
-//   }
-//   saturate(value = 1) {
-//     this.multiply([
-//       0.213 + 0.787 * value,
-//       0.715 - 0.715 * value,
-//       0.072 - 0.072 * value,
-//       0.213 - 0.213 * value,
-//       0.715 + 0.285 * value,
-//       0.072 - 0.072 * value,
-//       0.213 - 0.213 * value,
-//       0.715 - 0.715 * value,
-//       0.072 + 0.928 * value,
-//     ]);
-//   }
-//   multiply(matrix) {
-//     const newR = this.clamp(this.r * matrix[0] + this.g * matrix[1] + this.b * matrix[2]);
-//     const newG = this.clamp(this.r * matrix[3] + this.g * matrix[4] + this.b * matrix[5]);
-//     const newB = this.clamp(this.r * matrix[6] + this.g * matrix[7] + this.b * matrix[8]);
-//     this.setState({r: newR, g: newG, b: newB});
-//   }
-//   brightness(value = 1) {
-//     this.linear(value);
-//   }
-//   contrast(value = 1) {
-//     this.linear(value, -(0.5 * value) + 0.5);
-//   }
-//   linear(slope = 1, intercept = 0) {
-//        this.setState({r: this.clamp(this.r * slope + intercept * 255), g: this.clamp(this.g * slope + intercept * 255), b: this.clamp(this.b * slope + intercept * 255)});
-//   }
-//   invert(value = 1) {
-//       this.setState({r: this.clamp((value + this.r / 255 * (1 - 2 * value)) * 255), g: this.clamp((value + this.g / 255 * (1 - 2 * value)) * 255), b: this.clamp((value + this.b / 255 * (1 - 2 * value)) * 255)});
-//   }
-//   hsl() {
-//     const r = this.r / 255;
-//     const g = this.g / 255;
-//     const b = this.b / 255;
-//     const max = Math.max(r, g, b);
-//     const min = Math.min(r, g, b);
-//     let h, s, l = (max + min) / 2;
-//     if (max === min) {
-//       h = s = 0;
-//     } else {
-//       const d = max - min;
-//       s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-//       switch (max) {
-//         case r:
-//           h = (g - b) / d + (g < b ? 6 : 0);
-//           break;
-//         case g:
-//           h = (b - r) / d + 2;
-//           break;
-//         case b:
-//           h = (r - g) / d + 4;
-//           break;
-//       }
-//       h /= 6;
-//     }
-//     return {
-//       h: h * 100,
-//       s: s * 100,
-//       l: l * 100,
-//     };
-//   }
-//   clamp(value) {
-//     if (value > 255) {
-//       value = 255;
-//     } else if (value < 0) {
-//       value = 0;
-//     }
-//     return value;
-//   }
-// }
-// export default Color;
+"use strict";
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.hexToRgb = hexToRgb;
+
+function hexToRgb(hex) {
+  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+    return r + r + g + g + b + b;
+  });
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)] : null;
+}
 "use strict";
 "use strict";
 
@@ -135,6 +23,8 @@ Object.defineProperty(exports, "__esModule", {
 exports["default"] = void 0;
 
 var _react = _interopRequireWildcard(require("react"));
+
+var _hexToRgb = require("./src/colorConverter/hexToRgb");
 
 function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
 
@@ -174,16 +64,6 @@ var IMG = /*#__PURE__*/function (_Component) {
   }
 
   _createClass(IMG, [{
-    key: "hexToRgb",
-    value: function hexToRgb(hex) {
-      var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-      hex = hex.replace(shorthandRegex, function (m, r, g, b) {
-        return r + r + g + g + b + b;
-      });
-      var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-      return result ? [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)] : null;
-    }
-  }, {
     key: "render",
     value: function render() {
       var namedColors = {
@@ -343,9 +223,9 @@ var IMG = /*#__PURE__*/function (_Component) {
         desiredColor = this.props.color;
       } else if (typeof this.props.color == 'string') {
         if (this.props.color.includes('#')) {
-          desiredColor = this.hexToRgb(this.props.color);
+          desiredColor = (0, _hexToRgb.hexToRgb)(this.props.color);
         } else if (!this.props.color.includes('#') && namedColors[this.props.color]) {
-          desiredColor = this.hexToRgb(namedColors[this.props.color]);
+          desiredColor = (0, _hexToRgb.hexToRgb)(namedColors[this.props.color]);
         }
       }
 
