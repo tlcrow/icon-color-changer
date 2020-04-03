01a6d467063a41753bf1cfb0a416230a8ab6ca05
TLC <pi_lover852@yahoo.com>
2020-04-03 10:13:24 -0600

    moved hsl class to separate file

--- index.js
+++ index.js
@@ -15,24 +15,11 @@ function hexToRgb(hex) {
   return result ? [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)] : null;
 }
 "use strict";
-"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports["default"] = void 0;
-
-var _react = _interopRequireWildcard(require("react"));
-
-var _hexToRgb = require("./src/colorConverter/hexToRgb");
-
-var _variables = require("./src/variables");
-
-function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
-
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+exports.HSLValues = void 0;
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -40,79 +27,14 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-function _createSuper(Derived) { return function () { var Super = _getPrototypeOf(Derived), result; if (_isNativeReflectConstruct()) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-var IMG = /*#__PURE__*/function (_Component) {
-  _inherits(IMG, _Component);
-
-  var _super = _createSuper(IMG);
-
-  function IMG(props) {
-    _classCallCheck(this, IMG);
-
-    return _super.call(this, props);
-  }
-
-  _createClass(IMG, [{
-    key: "render",
-    value: function render() {
-      var desiredColor;
-      var resultingColor;
-
-      if (Array.isArray(this.props.color)) {
-        desiredColor = this.props.color;
-      } else if (typeof this.props.color == 'string') {
-        if (this.props.color.includes('#')) {
-          desiredColor = (0, _hexToRgb.hexToRgb)(this.props.color);
-        } else if (!this.props.color.includes('#') && _variables.namedColors[this.props.color]) {
-          desiredColor = (0, _hexToRgb.hexToRgb)(_variables.namedColors[this.props.color]);
-        }
-      }
-
-      if (Array.isArray(desiredColor) && desiredColor.length == 3) {
-        var processingColor = new Color(desiredColor[0], desiredColor[1], desiredColor[2]);
-        var finishingColor = new Solver(processingColor);
-        resultingColor = finishingColor.solve();
-      } else {
-        resultingColor = {
-          filter: ''
-        };
-      }
-
-      return /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement("img", {
-        src: this.props.image,
-        style: {
-          filter: " ".concat(resultingColor.filter)
-        }
-      }));
-    }
-  }]);
-
-  return IMG;
-}(_react.Component);
-
-exports["default"] = IMG;
-
-var Color = /*#__PURE__*/function () {
-  function Color(r, g, b) {
-    _classCallCheck(this, Color);
+var HSLValues = /*#__PURE__*/function () {
+  function HSLValues(r, g, b) {
+    _classCallCheck(this, HSLValues);
 
     this.set(r, g, b);
   }
 
-  _createClass(Color, [{
+  _createClass(HSLValues, [{
     key: "toString",
     value: function toString() {
       return "rgb(".concat(Math.round(this.r), ", ").concat(Math.round(this.g), ", ").concat(Math.round(this.b), ")");
@@ -245,21 +167,114 @@ var Color = /*#__PURE__*/function () {
     }
   }]);
 
-  return Color;
+  return HSLValues;
 }();
 
-var Solver = /*#__PURE__*/function () {
-  function Solver(target, baseColor) {
-    _classCallCheck(this, Solver);
+exports.HSLValues = HSLValues;
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireWildcard(require("react"));
+
+var _hexToRgb = require("./src/colorConverter/hexToRgb");
+
+var _variables = require("./src/variables");
+
+var _hslValues = require("./src/colorConverter/hslValues");
+
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _createSuper(Derived) { return function () { var Super = _getPrototypeOf(Derived), result; if (_isNativeReflectConstruct()) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+var IMG = /*#__PURE__*/function (_Component) {
+  _inherits(IMG, _Component);
+
+  var _super = _createSuper(IMG);
+
+  function IMG(props) {
+    _classCallCheck(this, IMG);
+
+    return _super.call(this, props);
+  }
+
+  _createClass(IMG, [{
+    key: "render",
+    value: function render() {
+      var desiredColor;
+      var resultingColor;
+
+      if (Array.isArray(this.props.color)) {
+        desiredColor = this.props.color;
+      } else if (typeof this.props.color == 'string') {
+        if (this.props.color.includes('#')) {
+          desiredColor = (0, _hexToRgb.hexToRgb)(this.props.color);
+        } else if (!this.props.color.includes('#') && _variables.namedColors[this.props.color]) {
+          desiredColor = (0, _hexToRgb.hexToRgb)(_variables.namedColors[this.props.color]);
+        }
+      }
+
+      if (Array.isArray(desiredColor) && desiredColor.length == 3) {
+        var processingColor = new _hslValues.HSLValues(desiredColor[0], desiredColor[1], desiredColor[2]);
+        var finishingColor = new FilterValues(processingColor);
+        resultingColor = finishingColor.solve();
+      } else {
+        resultingColor = {
+          filter: ''
+        };
+      }
+
+      return /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement("img", {
+        src: this.props.image,
+        style: {
+          filter: " ".concat(resultingColor.filter)
+        }
+      }));
+    }
+  }]);
+
+  return IMG;
+}(_react.Component);
+
+exports["default"] = IMG;
+
+var FilterValues = /*#__PURE__*/function () {
+  function FilterValues(target, baseColor) {
+    _classCallCheck(this, FilterValues);
 
     this.target = target;
     this.targetHSL = target.hsl();
-    this.reusedColor = new Color(0, 0, 0);
+    this.reusedColor = new _hslValues.HSLValues(0, 0, 0);
     console.log(target);
     console.log(baseColor);
   }
 
-  _createClass(Solver, [{
+  _createClass(FilterValues, [{
     key: "solve",
     value: function solve() {
       var result = this.solveNarrow(this.solveWide());
@@ -399,7 +414,7 @@ var Solver = /*#__PURE__*/function () {
     }
   }]);
 
-  return Solver;
+  return FilterValues;
 }(); // 'invert(19%) sepia(86%) saturate(6794%) hue-rotate(275deg) brightness(69%) contrast(105%)'
 "use strict";
 
